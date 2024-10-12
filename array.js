7bf8e15ea5c361e21fb8fa484c0c8fc5bb8d3d4e
Anish003-code <Anishkumarm883@gmail.com>
2024-10-12 16:54:06 +0530

    prac

--- array.js
+++ array.js
@@ -1,4 +1,4 @@
-let marks_12 =  [11,22,11,11,11,1111,111,11,11, null, false];
+/* let marks_12 =  [11,22,11,11,11,1111,111,11,11, null, false];
 console.log(marks_12);
 // in  arrays we can put different types of values 
 console.log(marks_12[3]);
@@ -49,10 +49,53 @@ console.log(e); // it will sort according to the digits first word like 222 come
 // three arguments of splice 1st= konse index ,2nd = kinte elements ko remove krna h, 3rd = konse elements add krne hai
 let w = [11,11,1,11,1,1111,11,111];
 w.splice(2,4,101,112,11,"ok", 222);
-console.log(w);
+console.log(w); 
+                         // LOOPS in the array
+// loops in the array
+let num = [22,111,3333,1]
+for(let i = 0; i < num.length;i++){
+  console.log(num[i]);
+}
+num.forEach((Elementt) => { // it will run for each elements
+    console.log(Elementt*Elementt)
+});
+// ARRAY.from
 
+let namee = "anish";
+let arr = Array.from(namee); // it is saying make an array from the string(namee) 
+console.log(arr); //o/p => ['a','n','i','s','h']
 
+// for...of loop   it is a kind of short cut 
+ for(let i of num){
+    console.log(i); 
+ 
+              // map
+ // map creates a new array and for each does not create a  new array
+let arr = [11,342,22]
+ let a = arr.map((value,index,array) => {
+    console.log(value,index,array) // this will give the value along with its index and the whole array as map have three arguments
+    return value + 1; // nya array bna do aur un subme ek ek number add krdo
+ })
+console.log(a)
+// array filter methods   actually me filter ki trh kam krta hai
+let arr2 = [1,2,3,44,5,,6,7,8,9,10]
+let a2= arr2.filter((a) => {
+    console.log(a)
+    return a<5
+})
+console.log(a2) // orignal array do not change
 
+/* format let newArray = array.filter(function(element) {
+    return condition;
+  });  */
+  
+  //reduced method 
+  
+let arr3 = [1,2,3,4,2,2,1]
+let newarr3 = arr3.reduce((h1,h2) => {
+    return h1 * h2
+})
+console.log(newarr3);
 
 
 
--- stringsprac.js
+++ stringsprac.js
@@ -1,1 +1,24 @@
-
+// loops in the array
+let num = [22,1,4,2,1]
+for(let i = 0; i < 3; i++){
+    console.log(num[i])
+}
+// map array 
+let array = [1,2,3,4,5,11]
+let newarray = array.map((num)=>{
+    return num *num *num
+})
+console.log(newarray);
+console.log(array);
+ let oldarray = [2,3,4,5,5]
+ for(let i = 0; i < 4; i++){
+    console.log(oldarray[i*i]);
+ }
+ // using the for each loop
+ oldarray.forEach((num)=> {
+    console.log(num*num)
+ });
+ // it makes array from the string
+ let namee = "anish"
+  let a = Array.from(namee);
+  console.log(a[3]);
